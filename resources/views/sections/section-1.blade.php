58085c463a916e28940c6c8284e6207f97436d19
BRAHIM OUDRA <126688886+Oudra1729@users.noreply.github.com>
2024-05-26 23:11:59 +0100

    css

--- resources/views/sections/section-1.blade.php
+++ resources/views/sections/section-1.blade.php
@@ -70,4 +70,34 @@
             e
         });
     });
+
+
+
+    $(document).ready(function() {
+        $('#property-select').select2({
+            tags: true,
+            maximumSelectionLength: 5,
+            placeholder: 'Choisissez votre type',
+            allowClear: true,
+            createTag: function(params) {
+                return {
+                    id: params.term,
+                    text: params.term,
+                    newOption: true
+                };
+            },
+            templateResult: function(data) {
+                var $result = $("<span></span>");
+
+                $result.text(data.text);
+
+                if (data.newOption) {
+                    $result.append(" <em>(new)</em>");
+                }
+
+                return $result;
+            }
+        });
+    });
+
 </script>
--- resources/views/sections/section-2.blade.php
+++ resources/views/sections/section-2.blade.php
@@ -6,7 +6,7 @@
     <title>My Page</title>
     {{-- Uncomment the following line if you're using Bootstrap --}}
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> --}}
-    {{-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet"> --}}
+
     <link rel="stylesheet" href="{{ asset('css/tilwind.css') }}">
 
     <style>
