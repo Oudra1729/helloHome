9fbcd846f7e61fab87663416ef4a9cf6fba53cae
BRAHIM OUDRA <126688886+Oudra1729@users.noreply.github.com>
2024-05-27 08:03:11 +0100

    xihaja

--- resources/views/sections/section-1.blade.php
+++ resources/views/sections/section-1.blade.php
@@ -1,3 +1,4 @@
+
 <section class="section-1">
     <div>
         <div class="titles">
@@ -24,6 +25,13 @@
             <div class="search">
                 <div class="input-decorated">
                     <img src="{{ asset('assets/icons/location1.png') }}" alt="icon" class="icon" />
+                    {{-- <select name="type" id="property-select" style="width: 100%;">
+                        <option value="">Choisissez votre type</option>
+                        @foreach ($properties as $property)
+                        <option value="{{ $property->city }}">{{ $property->city }}</option>
+                        @endforeach
+                    </select> --}}
+
                     <input   type="text" name="city" id="city" placeholder="Ville" />
                 </div>
                 <div class="separator"></div>
@@ -37,19 +45,20 @@
                     </select>
                     {{-- <input type="text" placeholder="Type de bien (ex: maison, appartement, terrain, etc.)" name="type" id="type" /> --}}
                 </div>
+
                 <div class="separator"></div>
                 <div class="input-decorated">
                     <img src="{{ asset('assets/icons/price.png') }}" alt="icon" class="icon" />
                     <div id="debt-amount-slider">
-                        <input type="radio" name="debt-amount" id="1" value="1" required>
+                        <input type="radio" name="debt-amount" id="1" value="5000" required>
                         <label for="1" data-debt-amount="< $10k"></label>
-                        <input type="radio" name="debt-amount" id="2" value="2" required>
+                        <input type="radio" name="debt-amount" id="2" value="50000" required>
                         <label for="2" data-debt-amount="$10k-25k"></label>
-                        <input type="radio" name="debt-amount" id="3" value="3" required>
+                        <input type="radio" name="debt-amount" id="3" value="300000" required>
                         <label for="3" data-debt-amount="$25k-50k"></label>
-                        <input type="radio" name="debt-amount" id="4" value="4" required>
+                        <input type="radio" name="debt-amount" id="4" value="1000000" required>
                         <label for="4" data-debt-amount="$50k-100k"></label>
-                        <input type="radio" name="debt-amount" id="5" value="5" required>
+                        <input type="radio" name="debt-amount" id="5" value="50000000" required>
                         <label for="5" data-debt-amount="$100k+"></label>
                         {{-- <div id="debt-amount-pos"></div> --}}
                     </div>
@@ -59,48 +68,7 @@
         </form>
     </div>
 </section>
-{{--
-<script>
-    document.querySelectorAll('.section-1 .actions .choices label').forEach(label => {
-        label.addEventListener('click', function() {
-            // Remove 'clicked' class from all labels
-            document.querySelectorAll('.section-1 .actions .choices label').forEach(lbl => lbl.classList.remove('clicked'));
 
-            // Add 'clicked' class to the clicked label
-            e
-        });
-    });
-
-
-
-    $(document).ready(function() {
-        $('#property-select').select2({
-            tags: true,
-            maximumSelectionLength: 5,
-            placeholder: 'Choisissez votre type',
-            allowClear: true,
-            createTag: function(params) {
-                return {
-                    id: params.term,
-                    text: params.term,
-                    newOption: true
-                };
-            },
-            templateResult: function(data) {
-                var $result = $("<span></span>");
-
-                $result.text(data.text);
-
-                if (data.newOption) {
-                    $result.append(" <em>(new)</em>");
-                }
-
-                return $result;
-            }
-        });
-    });
-
-</script> --}}
 <script>
     document.querySelectorAll('.section-1 .actions .choices label').forEach(label => {
         label.addEventListener('click', function() {
@@ -153,6 +121,3 @@ $(document).ready(function() {
     });
 </script>
 
-
-
-{{-- ghjkhhhhhhh --}}
