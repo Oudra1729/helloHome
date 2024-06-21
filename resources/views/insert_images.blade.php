b3e9a4541c712f74b991c7a4b51e30b4111f2047
BRAHIM OUDRA <126688886+Oudra1729@users.noreply.github.com>
2024-06-21 10:07:12 +0100

    xihaja

--- resources/views/insert_images.blade.php
+++ resources/views/insert_images.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 @section('content')
 
 <h1>image form</h1>
@@ -24,4 +24,30 @@
 </form>
 
 
+@endsection --}}
+
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Insérer d'image</h1>
+<form action="{{ route('images.store') }}" method="POST" enctype="multipart/form-data" class="custom-form">
+    @csrf
+
+    <!-- Input field for property ID -->
+    <input type="hidden" name="property_id" value="{{ $properties->id }}">
+
+    <!-- Affichage de l'identifiant de la propriété pour vérification -->
+    {{-- <p>Identifiant de la propriété : {{ $properties->id }}</p> --}}
+
+    <!-- Champ pour le fichier image -->
+    <div class="form-group">
+        <label for="image_path">Image :</label>
+        <input type="file" id="image_path" name="image_path[]" multiple>
+    </div>
+
+    <button type="submit" class="btn-submit">Soumettre</button>
+</form>
+
 @endsection
+
